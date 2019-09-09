f9c2ffcf2f25052fffc50390ba6ddbebad654860
W. Alex Best <amp@amperture.com>
2019-09-08 21:11:19 -0500

    Models framed out

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
 
-from app import routes  # noqa: E402,F401
+from app import routes, models  # noqa: E402,F401
---
+++ app/models.py
@@ -0,0 +1,41 @@
+from app import app, db  # noqa: F401
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    username = db.Column(db.String(64), unique=True)
+    '''
+    TODO: invoices: one-to-many Invoice objects
+    TODO: client: one-to-one BTCPayClient
+    TODO: integration: one-to-??? StreamElements/StreamLabs
+    '''
+
+
+class Invoice(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    # status holds valid values "paid", "unpaid", "expired"
+    status = db.Column(db.Integer)
+
+    message = db.Column(db.String(255))
+    email = db.Column(db.String(255))
+    username = db.Column(db.String(255))
+
+    '''
+    TODO: client, many-to-one BTCPayClient
+    TODO: user, many-to-one
+    '''
+
+
+class BTCPayClient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    client = db.Column(db.PickleType)
+    '''
+    user: one-to-one User
+    '''
+
+
+class StreamElementsIntegration(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    jwt = db.Column(db.String(1024))
