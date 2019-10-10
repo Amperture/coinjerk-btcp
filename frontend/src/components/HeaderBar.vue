fe6ea0fec7e33f53bee1ee8f5d0d0fafc5337021
W. Alex Best <amp@amperture.com>
2019-10-10 06:55:25 -0500

    HeaderBar design incoming

---
+++ frontend/src/components/HeaderBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <v-navigation-drawer
+      v-model="navDrawer.model"
+      bottom
+      clipped
+      app
+    ></v-navigation-drawer>
+    <v-app-bar
+      clipped-left
+      dark
+      color="accent"
+      app
+    >
+        <v-app-bar-nav-icon
+          v-if="navDrawer.type !== 'permanent'"
+          @click.stop="navDrawer.model = !navDrawer.model"
+        ></v-app-bar-nav-icon>
+        <img 
+          src="@/assets/coinjerk-header.png"
+          style="max-height:100%"
+          @click="$router.push('/')"
+        />
+        <div class='flex-grow-1'></div>
+        <v-switch
+          v-model="$vuetify.theme.dark"
+          color="primary"
+          prepend-icon="mdi-brightness-7"
+          append-icon="mdi-brightness-3"
+          hide-details
+        ></v-switch>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'header-bar',
+
+  components: {
+  },
+
+  data: () => ({
+    navDrawer: {
+      model: null,
+      clipped: true,
+      floating: false,
+      mini: false,
+    },
+  })
+}
+</script>
