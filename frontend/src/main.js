a9646431e5d4bd2b9e14cdb4efc4208dc3018d69
W. Alex Best <amp@amperture.com>
2019-10-13 00:47:22 -0500

    Organization

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
-import store from './store'
-import router from './router'
+import vuetify from '@/plugins/vuetify';
+import store from '@/store/'
+import router from '@/router/'
 import currency from 'v-currency-field'
 
 import 'v-currency-field/dist/index.css'
