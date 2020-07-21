b981117fe2fbe9af08c2cb3eab6337923429da95
W. Alex Best <amp@amperture.com>
2020-07-20 23:03:21 -0500

    Visual Design and Rules for Tip Page

--- frontend/src/components/TipFormCard.vue
+++ frontend/src/components/TipFormCard.vue
@@ -17,28 +17,37 @@
     <v-card-text>
       <v-form>
         <v-row>
-          <v-col cols="8">
-          <v-text-field
-            label="Name"
-            name="login"
-            prepend-icon="mdi-account-box"
-            type="text"
-            clearable
-            hint="Optional. Twitch or Twitter name preferred."
-          />
+          <v-col cols="12">
+            <v-text-field
+              label="Name"
+              name="login"
+              prepend-icon="mdi-account-box"
+              type="text"
+              clearable
+              hint="Optional. Twitch or Twitter name preferred."
+              :rules='[rules.nameMaxLength, rules.nameAlphaNumeric]'
+            />
           </v-col>
+        </v-row>
+        <v-row>
           <v-col cols="4">
             <v-select
               :items="currencies"
-              label="Currency/Denomination"
+              label="Denomination"
               prepend-icon="mdi-currency-usd"
               />
           </v-col>
-        </v-row>
-        <v-row>
-          Choose Amount Here
-        </v-row>
-        <v-row>
+          <v-col>
+            <v-text-field
+              label='Amount'
+              v-model="height"
+              min="0"
+              step=".1"
+              type="number"
+            ></v-text-field>
+          </v-col>
+          </v-row>
+          <v-row>
           <v-col cols="12">
             <v-textarea
               auto-grow="true"
@@ -46,6 +55,8 @@
               prepend-icon="mdi-comment-text"
               label="Message"
               hint="Would you like to leave a message for the broadcaster? You can do so here!"
+              counter
+              :rules='[rules.messageMaxLength]'
               />
           </v-col>
         </v-row>
@@ -59,6 +70,25 @@
 </template>
 
 <script>
+  export default {
+    data: () => ({
+      rules: {
+        required: value => !!value || 'This field is required!',
+        nameMaxLength: value => value.length <= 25 || "No more than 25 characters!",
+        nameAlphaNumeric: value => {
+          const pattern = /^[a-zA-Z0-9]*$/
+          return pattern.test(value) || "Letters and Numbers only, sorry."
+        },
+        messageMaxLength: value => value.length <= 255 || "No more than 255 characters!",
+      },
+      drawer: false,
+      currencies: [
+        'USD',
+        'BTC',
+        'satoshis'
+      ]
+    }),
+  }
 </script>
 
 <style>
